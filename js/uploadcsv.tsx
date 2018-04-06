2f6966befb8f5b358d0794406c7ab6a23c2639bd
Kenzie Togami <ket1999@gmail.com>
2018-04-05 21:41:51 -0700

    be user friendly, allow clicking

--- js/uploadcsv.tsx
+++ js/uploadcsv.tsx
@@ -4,52 +4,97 @@ import {Actions, addAndSelectTab, ISTATE} from "./reduxish/store";
 import {CsvData} from "./csvData";
 import {CsvTabProps} from "./tabTypes";
 
-function pullFiles(dt: DataTransfer): File[] {
-    if (dt.items) {
-        return Array.from(dt.items)
-            .map(d => d.getAsFile())
-            .filter(isDefined);
-    }
-    return Array.from(dt.items);
-}
+export class UploadCsv extends React.Component<{}, {
+    hovered: boolean
+    fileValue?: File
+}> {
 
-function onDrop(e: React.DragEvent<HTMLElement>) {
-    e.preventDefault();
-    onExit(e);
-    const files = pullFiles(e.dataTransfer);
-    // assume first file is the csv data
-    let csvFile = files[0];
-    if (!csvFile.name.endsWith('.csv')) {
-        return;
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            hovered: false
+        }
     }
 
-    const reader = new FileReader();
-    reader.onload = () => {
-        ISTATE.dispatch(Actions.removeAllTabs(undefined));
-        addAndSelectTab(new CsvTabProps(CsvData.parse(reader.result)));
-    };
-    reader.readAsText(csvFile);
-}
+    pullFiles(dt: DataTransfer): File[] {
+        if (dt.items) {
+            return Array.from(dt.items)
+                .map(d => d.getAsFile())
+                .filter(isDefined);
+        }
+        return Array.from(dt.items);
+    }
 
-function onDragOver(e: React.DragEvent<HTMLElement>) {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'copy';
-}
+    onDrop(e: React.DragEvent<HTMLElement>) {
+        e.preventDefault();
+        this.onExit();
+        const files = this.pullFiles(e.dataTransfer);
+        this.onFileUpload(files);
+    }
 
-function onEnter(e: React.DragEvent<HTMLElement>) {
-    e.currentTarget.classList.add('bg-primary');
-    e.currentTarget.classList.remove('bg-info');
-}
+    onFileUpload(files: File[]) {
+        // assume first file is the csv data
+        let csvFile = files[0];
+        if (!csvFile || !csvFile.name.endsWith('.csv')) {
+            return;
+        }
 
-function onExit(e: React.DragEvent<HTMLElement>) {
-    e.currentTarget.classList.remove('bg-primary');
-    e.currentTarget.classList.add('bg-info');
-}
+        const reader = new FileReader();
+        reader.onload = () => {
+            ISTATE.dispatch(Actions.removeAllTabs(undefined));
+            addAndSelectTab(new CsvTabProps(CsvData.parse(reader.result)));
+        };
+        reader.readAsText(csvFile);
+    }
 
-export function UploadCsv() {
-    return <i className="bg-info p-2 rounded fas fa-upload fa-3x" onDrop={onDrop}
-              onDragOver={onDragOver}
-              onDragEnter={onEnter}
-              onDragLeave={onExit}>
-    </i>;
+    onDragOver(e: React.DragEvent<HTMLElement>) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+    }
+
+    onEnter() {
+        this.setState(prevState => ({...prevState, hovered: true}));
+    }
+
+    onExit() {
+        this.setState(prevState => ({...prevState, hovered: false}));
+    }
+
+    onClick(e: React.MouseEvent<HTMLElement>) {
+        e.preventDefault();
+        // Click open the file input
+        let inputs = $(e.currentTarget).siblings('input');
+        inputs[0].click();
+    }
+
+    onFileChosen(e: JQuery.Event<HTMLElement, null>) {
+        const target = e.currentTarget;
+        if (target === null) {
+            return;
+        }
+        let fileList = (target as HTMLInputElement).files;
+        if (fileList === null) {
+            return;
+        }
+        this.onFileUpload(Array.from(fileList));
+    }
+
+    render() {
+        return <label className="m-0">
+            <i className={`${this.state.hovered ? 'bg-primary' : 'bg-secondary'} p-2 rounded fas fa-upload fa-3x`}
+               onDrop={this.onDrop.bind(this)}
+               onDragOver={this.onDragOver.bind(this)}
+               onDragEnter={this.onEnter.bind(this)}
+               onDragLeave={this.onExit.bind(this)}
+               onMouseEnter={this.onEnter.bind(this)}
+               onMouseLeave={this.onExit.bind(this)}
+               onClick={this.onClick.bind(this)}>
+            </i>
+            <input type="file" className="d-none" ref={(r: HTMLInputElement) => {
+                $(r).on('change', e => {
+                    this.onFileChosen(e);
+                });
+            }}/>
+        </label>;
+    }
 }
